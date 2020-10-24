23acdaaa5b44030f284535bb11884011a53de79f
ARSH SHARMA <184517@nith.ac.in>
2020-10-24 16:41:47 +0530

    Check ctrl_config in control gate
    nit

--- src/qobj.jl
+++ src/qobj.jl
@@ -46,6 +46,7 @@ function generate_inst(blk::PutBlock{N,M}) where {N,M}
 end
 
 function generate_inst(blk::ControlBlock{N,GT,C}) where {N,GT,C}
+    findfirst(!=(0),blk.ctrl_config) == nothing && error("Inverse Control used in Control gate context") 
 	generate_inst(blk.content, blk.locs, blk.ctrl_locs)
 end
 
